98689d0d0e9a4b64ea8857f4e8b6396a99928c39
EyadOmar <eiad.omar88@gmail.com>
2023-12-23 21:58:41 +0200

    added arabic to mainNav

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import PageHeader from './layout/PageHeader';
 
 function App() {
   return (
-    <div className=" font-body min-h-screen bg-bgk dark:bg-black dark:text-white transition-colors flex flex-col ">
+    <div className="font-body min-h-screen bg-bgk dark:bg-black dark:text-white transition-colors flex flex-col ">
       <NavMenuContextProvider>
         <PageHeader />
         <div className=" flex-1 overflow-hidden">
