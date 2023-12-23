4af8a9c411228eebebe55b2a2ec122b87fbac6fb
EyadOmar <eiad.omar88@gmail.com>
2023-12-24 00:01:58 +0200

    home is done

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -1,9 +1,11 @@
 import { motion } from 'framer-motion';
 import { useNavMenuContext } from '../contexts/NavMenuContext';
 import { GalleryVerticalEnd, LucideIcon, User } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 
 function Home() {
   const { isNavOpen } = useNavMenuContext();
+  const { t } = useTranslation();
   const sectionVariant = {
     invisible: {
       opacity: 0,
@@ -23,15 +25,12 @@ function Home() {
       layout
     >
       <article>
-        <h1 className="text-5xl lg:text-7xl">Front-End Developer</h1>
-        <p className=" mt-10  text-xl lg:text-3xl">
-          Eyad Omar, web developer that aspires to design and build attractive
-          modern websites.
-        </p>
+        <h1 className="text-5xl lg:text-7xl">{t('job')}</h1>
+        <p className=" mt-10  text-xl lg:text-3xl">{t('slogan')}</p>
       </article>
       <div className="flex gap-4 mt-10 text-xl lg:text-2xl">
-        <HomeButton Icon={GalleryVerticalEnd} title="Projects" />
-        <HomeButton Icon={User} title="Contact" />
+        <HomeButton Icon={GalleryVerticalEnd} title="projects" />
+        <HomeButton Icon={User} title="contact" />
       </div>
     </motion.section>
   );
@@ -43,10 +42,11 @@ type HomeButtonProps = {
 };
 
 function HomeButton({ Icon, title }: HomeButtonProps) {
+  const { t } = useTranslation();
   return (
     <motion.button className=" flex gap-2 items-center border-2 border-black dark:border-white rounded-lg p-3 outline-none hover:text-bgk hover:bg-black dark:hover:text-black dark:hover:bg-bgk transition-colors duration-300">
       <Icon />
-      {title}
+      {t(title)}
     </motion.button>
   );
 }
